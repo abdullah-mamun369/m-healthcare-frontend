9ad584e564a36cc3b57c9e2a15b32c9b2062b360
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-25 00:13:38 +0600

    Basic navbar is done

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,5 +1,45 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="h4" component={Link} href="/" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H
+          </Box>{" "}
+          Health Care
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography component={Link} href="/">
+            Health Plans
+          </Typography>
+          <Typography component={Link} href="/">
+            Medicine
+          </Typography>
+          <Typography component={Link} href="/">
+            Diagnosistics
+          </Typography>
+          <Typography component={Link} href="/">
+            NGOs
+          </Typography>
+        </Stack>
+        <Button component={Link} href="/login">
+          Login
+        </Button>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Navbar;

48e9904a775ef12d306f0da851e90992a164f3a7
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-26 23:18:42 +0600

    Specialist section is added to home section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Ui/Homepage/HeroSection/HeroSection";
+import Specialist from "@/components/Ui/Homepage/Specialist/Specialist";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 }

0c6f7b3a71c590ad1963ec03e84bb83ff5c86cec
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-27 17:51:50 +0600

    Why you choose us is done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/Ui/Homepage/HeroSection/HeroSection";
 import Specialist from "@/components/Ui/Homepage/Specialist/Specialist";
 import TopRatedDoctors from "@/components/Ui/Homepage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/Ui/Homepage/WhyUs/WhyUs";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <Specialist />
       <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 }

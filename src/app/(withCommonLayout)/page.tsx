7f41da24a7194439208120bbf883f563b47a4166
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-27 14:50:28 +0600

    Top rated doctor section is done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/Ui/Homepage/HeroSection/HeroSection";
 import Specialist from "@/components/Ui/Homepage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/Ui/Homepage/TopRatedDoctors/TopRatedDoctors";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 }

f484b03e60bbd8bbc641a68dd2f3513e3f8663af
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-29 18:36:13 +0600

    Basic box is done for register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import { Box, Container, Stack } from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
 
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 

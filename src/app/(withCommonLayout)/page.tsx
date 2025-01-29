aa4edec60b6f1578a1518269188ae6c69503a7d6
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-29 17:54:23 +0600

    How it works is done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/Ui/Homepage/HeroSection/HeroSection";
+import HowItWorks from "@/components/Ui/Homepage/HowItWorks/HowItWorks";
 import Specialist from "@/components/Ui/Homepage/Specialist/Specialist";
 import TopRatedDoctors from "@/components/Ui/Homepage/TopRatedDoctors/TopRatedDoctors";
 import WhyUs from "@/components/Ui/Homepage/WhyUs/WhyUs";
@@ -10,6 +11,7 @@ export default function Home() {
       <Specialist />
       <TopRatedDoctors />
       <WhyUs />
+      <HowItWorks />
     </>
   );
 }

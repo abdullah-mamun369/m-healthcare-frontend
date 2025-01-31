e3fda54ab3e1e23d21709404b9a74cc0ab698ebd
abdullah-mamun369 <maamamun369@gmail.com>
2025-02-01 00:01:48 +0600

    Form raper component is done

---
+++ src/components/Forms/PHForm.tsx
@@ -0,0 +1,25 @@
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+
+type TFormProps = {
+  children: React.ReactNode;
+};
+
+const PHForm = ({ children }: TFormProps) => {
+  const methods = useForm();
+  const { handleSubmit } = methods;
+
+  const submit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(submit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default PHForm;

8f6f2e3cbc256d3d493a562f5baf1371961b865e
abdullah-mamun369 <maamamun369@gmail.com>
2025-02-03 18:45:21 +0600

    Axios configuration is done with creating axios instance and modify the axios base query

---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,56 @@
+import { authKey } from "@/constant/authkey";
+import { IGenericErrorResponse, ResponseSuccessType } from "@/types";
+import { getFromLocalStorage } from "@/utils/local-storage";
+import axios from "axios";
+
+const instance = axios.create();
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers.post["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+export { instance };
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const accessToken = getFromLocalStorage(authKey);
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  //@ts-ignore
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    const responseObject: ResponseSuccessType = {
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    };
+
+    return responseObject;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const responseObject: IGenericErrorResponse = {
+      statusCode: error?.response?.data?.statusCode || 500,
+      message: error?.response?.data?.message || "Internal Server Error",
+      errorMessages: error?.response?.data?.messages,
+    };
+
+    // return Promise.reject(error);
+    return responseObject;
+  }
+);
